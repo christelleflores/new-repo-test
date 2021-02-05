207ecd1db9fea1da9c1d810c2985d603ef8d9034
christelle flores <57645973+christelleflores@users.noreply.github.com>
2021-02-04 20:00:35 -0500

    how are you

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 
 public class Hello {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Hello!");
+		System.out.println("How are you?");
+		String response = scanner.nextLine();
+		System.out.println("Great!");
 	}
 }
